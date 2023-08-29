fd32c934cbb0b04fe4eedfda5c500414962198aa
CJosueAK47 <carlos.jdc@discente.ufma.br>
2023-08-29 07:57:27 -0300

    pronto para postar

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for(let i = 0; i<buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
@@ -18,7 +19,18 @@ document.addEventListener('DOMContentLoaded', function(){
 
         })
     }
+
+    for(let i = 0; i< questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta)
+    }
 })
+
+function abreOuFechaResposta(elemento){
+    const classe = 'faq__questions__item--is-open';
+
+    const elementoPai = elemento.target.parentNode;
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
