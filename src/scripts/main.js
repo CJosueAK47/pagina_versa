8bee6f22cfde3db3a5ef1ca645deee347e8c12a0
CJosueAK47 <carlos.jdc@discente.ufma.br>
2023-10-11 00:08:00 -0300

    strong

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
